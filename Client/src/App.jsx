339303613ecc0e4209852e0263a7108d282b53f8
moralesmajo <00155020@uca.edu.sv>
2023-11-20 15:49:36 -0600

    feat: set up project for redesign

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,50 +1,11 @@
-import { Route, Routes, Navigate } from "react-router-dom"
-import Login from "./pages/Login/Login"
-import AllSongs from "./pages/AllSongs/AllSongs"
-import SongToPlaylist from "./pages/SongToPlaylist/SongToPlaylist"
-import SeePlaylists from "./pages/SeePlaylists/SeePlaylists"
-import PlaylistDetails from "./pages/PlaylistDetails/PlaylistDetails"
-import CreatePlaylist from "./pages/CreatePlaylist/CreatePlaylist"
-import Register from "./pages/Register/Register"
-import { useState } from "react"
-import MusicPlayer from "./pages/MusicPlayer/MusicPlayer"
-
-const isAuthenticated = () => {
-  const token = localStorage.getItem('token'); // Change 'token' to your specific token key
-
-  // Return true if the user is authenticated (token is present), false otherwise
-  return !!token;
-};
-
+import { Route, Routes } from "react-router-dom"
+import Dashboard from "./pages/Dashboard/Dashboard"
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(isAuthenticated());
-  
-  const handleLogin = () => {
-    setLoggedIn(true);
-  };
-  
+
   return (
     <Routes>
-      <Route path="/register" element={<Register />} /> {/* http://localhost:8080/auth/signup */}
-      <Route path="/login" element={<Login onLogin={handleLogin} />} /> {/* http://localhost:8080/auth/login */}
-      <Route path="/all-songs" element={<AllSongs />} /> {/* http://localhost:8080/song/ & http://localhost:8080/song/?title=keyword */}
-      <Route path="/add-song/:code" element={<SongToPlaylist />} /> {/* http://localhost:8080/playlist/ & http://localhost:8080/playlist/?playlistCode=code */}
-      <Route path="/song/" element={<MusicPlayer/>}/>
-      <Route path="/all-playlists" element={<SeePlaylists />} /> {/* http://localhost:8080/user/playlist */}
-      <Route path="/playlist-details/:code" element={<PlaylistDetails />} /> {/* http://localhost:8080/playlist/?playlistCode=code */}
-      <Route path="/create-playlist" element={<CreatePlaylist />} /> {/* http://localhost:8080/playlist */}
-      {loggedIn ? (
-        <>
-          <Route path="/all-songs" element={<AllSongs />} /> {/* http://localhost:8080/song/ & http://localhost:8080/song/?title=keyword */}
-          <Route path="/song/:code" element={<SongToPlaylist />} /> {/* http://localhost:8080/playlist/ & http://localhost:8080/playlist/?playlistCode=code */}
-          <Route path="/all-playlists" element={<SeePlaylists />} /> {/* http://localhost:8080/user/playlist */}
-          <Route path="/playlist-details/:code" element={<PlaylistDetails />} /> {/* http://localhost:8080/playlist/?playlistCode=code */}
-          <Route path="/create-playlist" element={<CreatePlaylist />} /> {/* http://localhost:8080/playlist */}
-        </>
-      ) : (
-        <Route element={<Navigate to="/login" />} />
-      )}
+      <Route path="/" element={ <Dashboard /> } />
     </Routes>
   )
 }
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -2,11 +2,41 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: '0.750rem',
+      base: '1rem',
+      xl: '1.333rem',
+      '2xl': '1.777rem',
+      '3xl': '2.369rem',
+      '4xl': '3.158rem',
+      '5xl': '4.210rem',
+    },
+    extend: {
+      colors: {
+        "white": "#F1F1F1",
+        "gray": "#4D5B60",
+        "gray-900": "#121312",
+        "greenish-black": "#000501",
+        "light-black": "#202120",
+        "lightest-green": "#0C6049",
+        "light-green": "#12B587",
+        "dark-green": "#117056",
+        "darkest-green": "#0C6049",
+        "light-cyan": "#083B4A",
+        "dark-cyan": "#062D38",
+        "darkest-cyan": "#031013",
+        "blue": "#20bde9",
+        "light-blue": "#2288A3",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: [],
+  },
 }
 
