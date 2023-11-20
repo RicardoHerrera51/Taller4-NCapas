239d02d51c6804a5cc88628edc976376083e4637
moralesmajo <00155020@uca.edu.sv>
2023-11-20 16:05:56 -0600

    feat: add navbars

---
+++ Client/src/components/Navbars/MusicBar.jsx
@@ -0,0 +1,11 @@
+{/* Music player bar */ }
+
+function Navbar() {
+  return (
+    <section className="navbar bottom-0 lg:relative lg:top-0 z-50 btm-nav lg:navbar bg-light-black lg:bg-greenish-black lg:border-b lg:border-b-light-black lg:w-full">
+      Music Player
+    </section>
+  )
+}
+
+export default Navbar
--- Client/src/pages/Dashboard/Dashboard.jsx
+++ Client/src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,32 @@
+import MobNavbar from '../../components/Navbars/MobNavbar';
+import MusicBar from '../../components/Navbars/MusicBar';
+import Sidebar from '../../components/Navbars/Sidebar';
+
+
 export default function Home() {
   return (
-    <div>
-      Dashboard
+    <div className="drawer lg:drawer-open bg-greenish-black">
+      <input type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col max-h-screen">
+        {/* Mobile navbar and music player bar */}
+        <MobNavbar />
+        <MusicBar />
+
+        {/* Contenido*/}
+        <main className="lg:flex-1 h-screen  lg:h-full flex flex-col items-center imprima-400 text-white px-10 pt-10 pb-28 lg:p-10 gap-5 overflow-y-auto scrollbar">
+          <div className='flex lg:flex-row flex-col w-full imprima-700 lg:px-10 px-15 justify-between lg:items-center gap-4 pb-6'>
+            <a className='text-2xl'>Hola, usuario</a>
+            
+          </div>
+          <div className='flex justify-start w-full lg:px-10 px-15 imprima-700'>
+            <a className='text-2xl'>Canciones</a>
+          </div>
+          Contenido
+        </main>
+      </div>
+
+      {/* Sidebar on web */}
+      <Sidebar />
     </div>
   )
 }
