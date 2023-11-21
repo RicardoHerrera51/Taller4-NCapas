c37e9b510fbc0e9dac675183c80ddb88042803da
moralesmajo <00155020@uca.edu.sv>
2023-11-20 23:52:27 -0600

    feat: add view to see user playlists and its details

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import Dashboard from "./pages/Dashboard/Dashboard"
 import CreatePlaylist from "./pages/CreatePlaylist/CreatePlaylist"
+import MyPlaylists from "./pages/MyPlaylists/MyPlaylists"
+import PlaylistDetails from "./pages/PlaylistDetails/PlaylistDetails"
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route path="/" element={ <Dashboard /> } />
       <Route path="/create-pl" element={ <CreatePlaylist /> } />
+      <Route path="/my-pl" element={ <MyPlaylists /> } />
+      <Route path="/pl-details" element={ <PlaylistDetails /> } />
     </Routes>
   )
 }
