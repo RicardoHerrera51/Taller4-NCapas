db39b8cfcda591c755163a640392765a41b23690
moralesmajo <00155020@uca.edu.sv>
2023-11-21 21:27:14 -0600

    feat: register view

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,12 +5,14 @@ import MyPlaylists from "./pages/MyPlaylists/MyPlaylists"
 import PlaylistDetails from "./pages/PlaylistDetails/PlaylistDetails"
 import Profile from "./pages/Profile/Profile"
 import Login from "./pages/Login/Login"
+import Register from "./pages/Register/Register"
 
 function App() {
 
   return (
     <Routes>
       <Route path="/login" element={ <Login /> } />
+      <Route path="/register" element={ <Register /> } />
       <Route path="/" element={ <Dashboard /> } />
       <Route path="/create-pl" element={ <CreatePlaylist /> } />
       <Route path="/my-pl" element={ <MyPlaylists /> } />
--- Client/src/pages/Register/Register.jsx
+++ Client/src/pages/Register/Register.jsx
@@ -1,24 +1,27 @@
-export default function Login() {
+export default function Register() {
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 min-h-screen p-6 sm:p-0 bg-greenish-black'>
             <div className='hidden sm:block'>
                 <img className='max-h-screen w-full' src="/assets/loginImage.jpg" alt="sideImage" />
             </div>
 
-            <div className='bg-gray-800 flex flex-col justify-center p-6'>
+            <div className='flex flex-col justify-center p-6'>
                 <form className='max-w-[400px] w-full mx-auto bg-dark-cyan p-8 px-8 rounded-lg' >
-                    <h2 className='text-4xl text-white text-center imprima-700'>LOG IN</h2>
+                    <h2 className='text-4xl text-white text-center imprima-700'>REGISTRO</h2>
                     <div className='flex flex-col text-white py-2 imprima-400'>
                         <input className='w-full py-2 px-3 bg-light-cyan text-white rounded-lg focus:outline-none' type="text" placeholder="Usuario" required/>
+                    </div>
+                    <div className='flex flex-col text-white py-2 imprima-400'>
+                        <input className='w-full py-2 px-3 bg-light-cyan text-white rounded-lg focus:outline-none' type="text" placeholder="Email" required/>
                     </div>
                     <div className='flex flex-col text-white py-2 imprima-400'>
                         <input className='w-full py-2 px-3 bg-light-cyan text-white rounded-lg focus:outline-none' type="password" placeholder="Contraseña" required/>
                     </div>
                     <div className='text-center text-slate-300 p-2 imprima-400'>
-                        <p>¿No tienes una cuenta?{" "}<a href='/register' className='text-light-green underline'>Registrate</a></p>
+                        <p>¿Ya tienes una cuenta?{" "}<a href='/login' className='text-light-green underline'>Inicia sesión</a></p>
                     </div>
                     <button className='w-full my-5 py-2 bg-light-green hover:bg-dark-green active:bg-lightest-green text-white hover:text-white active:text-white imprima-400 rounded-full'>
-                        Inicia sesión
+                        Registrarse
                     </button>
                 </form>
             </div>
