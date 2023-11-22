f7955b724ee6d39bcef17854cbf8667a2db5fce9
Chriss6202 <00132720@uca.edu.sv>
2023-11-22 14:43:33 -0600

    FEAT: Fetch dashboard songs

--- Client/src/pages/Dashboard/Dashboard.jsx
+++ Client/src/pages/Dashboard/Dashboard.jsx
@@ -5,8 +5,52 @@ import SearchBar from '../../components/SearchBar/SearchBar';
 import SongCard from '../../components/SongCards/SongCard';
 import Titles from '../../components/Titles/Titles';
 
-export default function Home({ user = "usuario" }) {
-  const token = localStorage.getItem('token');
+import {getSongs} from "../../services/songService";
+import {useEffect, useState} from "react";
+
+export default function Home({user = "usuario"}) {
+
+  
+  const [loading, setLoading] = useState(false);
+  const [songs, setSongs] = useState([]);
+  const [selectedSong, setSelectedSong] = useState(null);
+
+  const [filterValue, setFilterValue] = useState("");
+
+
+  const getData = async () => {
+        
+    try {
+      
+      setLoading(true);
+    let response = await getSongs("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtb3JhbGVzbWoiLCJpYXQiOjE3MDA2MzY3NDUsImV4cCI6MTcwMTkzMjc0NX0.pVCc7qqWreFX_o0q5cVOUHhHG60gYxRTL4YThe7SmNk");
+    if (response) {
+      setSongs(response.content);
+      console.log(response.content);
+    } 
+    } catch (error) {
+    console.error('Error al obtener datos de la API:', error);
+    }
+};
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleSearchButtonClick = (inputValue) => {
+    // Actualiza el estado solo cuando se hace clic en el botón de búsqueda
+    setFilterValue(inputValue);
+  };
+
+  const handleSelectedSong = (song) => {
+    console.log(song);
+    setSelectedSong(song);  
+  };
+
+
+  const filteredSongs = songs.filter((song) =>
+  song.title.toLowerCase().includes(filterValue.toLowerCase())
+);
 
   return (
     <div className="drawer lg:drawer-open bg-greenish-black">
@@ -14,24 +58,19 @@ export default function Home({ user = "usuario" }) {
       <div className="drawer-content flex flex-col max-h-screen">
         {/* Mobile navbar and music player bar */}
         <MobNavbar />
-        <MusicBar />
+        <MusicBar song={selectedSong}/>
 
         {/* Contenido*/}
         <main className="lg:flex-1 h-screen  lg:h-full flex flex-col items-center imprima-400 text-white px-10 pt-10 pb-28 lg:p-10 gap-5 overflow-y-auto scrollbar">
           <div className='flex lg:flex-row flex-col w-full imprima-700 lg:px-10 px-15 justify-between lg:items-center gap-4 pb-6'>
             <a className='text-2xl'>Hola, {user}</a>
-            <SearchBar placeholder='Busca una cancion...' />
+            <SearchBar onSearch={handleSearchButtonClick}  onChange={(e) => setFilterValue(e.target.value)}
+              value={filterValue} placeholder='Busca una cancion...' />
           </div>
           <Titles title='Canciones' />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
-          <SongCard />
+          {filteredSongs.map((song) => (
+                 <SongCard  onClick={() => handleSelectedSong(song)}  key={song.code} cover={song.album_cover} artist={song.artist} song={song.title} duration={song.duration}/>
+              ))}
         </main>
       </div>
 
