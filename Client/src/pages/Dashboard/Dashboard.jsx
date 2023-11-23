0cc1c477ee925a9fdb8695cd9e078ccabb730e30
moralesmajo <00155020@uca.edu.sv>
2023-11-23 02:08:49 -0600

    feat: username in dashboard

--- Client/src/pages/Dashboard/Dashboard.jsx
+++ Client/src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
 import MobNavbar from '../../components/Navbars/MobNavbar';
 import MusicBar from '../../components/Navbars/MusicBar';
 import Sidebar from '../../components/Navbars/Sidebar';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import SongCard from '../../components/SongCards/SongCard';
 import Titles from '../../components/Titles/Titles';
+import { infoProfile } from '../../services/AuthServices';
 
-export default function Home({ user = "usuario" }) {
+export default function Home() {
   const token = localStorage.getItem('token');
+  const [userInfo, setUserInfo] = useState({ username: "usuario" });
+
+  const getData = async () => {
+    try {
+      const userInfo = await infoProfile(token);
+
+      setUserInfo(userInfo);
+    } catch (error) {
+      console.error('An error occurred while getting the username info:', error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="drawer lg:drawer-open bg-greenish-black">
@@ -19,7 +37,7 @@ export default function Home({ user = "usuario" }) {
         {/* Contenido*/}
         <main className="lg:flex-1 h-screen  lg:h-full flex flex-col items-center imprima-400 text-white px-10 pt-10 pb-28 lg:p-10 gap-5 overflow-y-auto scrollbar">
           <div className='flex lg:flex-row flex-col w-full imprima-700 lg:px-10 px-15 justify-between lg:items-center gap-4 pb-6'>
-            <a className='text-2xl'>Hola, {user}</a>
+            <a className='text-2xl'>Hola, {userInfo.username}</a>
             <SearchBar placeholder='Busca una cancion...' />
           </div>
           <Titles title='Canciones' />
